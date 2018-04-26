83220abb9cadf7d7034af92ff19e8d673cb53e27
BarbaraKosela <38699532+BarbaraKosela@users.noreply.github.com>
2018-04-26 14:28:06 -0300

    Exemplo For
    Execução de exemplos sobre For

---
+++ ExemploFor/ExemploFor/Exemplo03.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ExemploFor
+{
+    class Exemplo03
+    {
+        public Exemplo03()
+        {
+
+            Console.WriteLine("Digite um número para multiplicar: ");
+            int numero = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = -1000000; i < 1000000; i++)
+            {
+                int multiplicar = numero * i;
+                Console.WriteLine(String.Format("{0} x {1} = {2}", numero, i, multiplicar));
+            }
+        }
+        
+    }
+}
